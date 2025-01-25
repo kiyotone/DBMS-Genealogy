5c0f2cea5e30ea6ed563eae080156c4535babd02
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-01-25 13:35:31 +0545

    file structure fixed

--- app/family/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class FamilyCreate(BaseModel):
-    family_name: str
-    origin_country: str
-    description: str = None
--- app/relationships/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class RelationshipCreate(BaseModel):
-    person1_id: int
-    person2_id: int
-    relationship_type: str
-    status: str
