978b22a34e66d7258deeab3be71e229d55ec30e8
Tayama Kirati <tayamab2003@gmail.com>
2025-01-26 19:50:13 +0545

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
- import React from 'react';
- import Navbar from './components/Navbar';
- import styles from './App.module.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styles from './App.module.css';
 import Home from './components/Home';
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
 
- const App = () => {
-   return (
-     <div className={styles.app}>
-      <Navbar/>
-      <Home/>
-       
-     </div>
-   )
- }
- 
- export default App
- 
+const App =() =>{
+
+  return (
+    <Router>
+      <div className={styles.app}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styles from "./Signup.module.css";
+
+const Signup = () => {
+  return (
+    <div className={styles.signupcontainer}>
+      <div className={styles.signupcard}>
+        <div className={styles.logocontainer}>
+          <img src="models/images/GMSlogo.png" alt="GMS Logo" className={styles.logo} />
+          <h1 className={styles.apptitle}>GMS</h1>
+        </div>
+
+        <div className={styles.cardcontent}>
+          <h2 className={styles.signuptitle}>Sign Up</h2>
+          <form className={styles.signupform}>
+
+          <div className={styles.inputgroup}>
+              <input
+                type="text"
+                placeholder="FirstName"
+                className={styles.inputfield}
+              />
+            </div>
+            <div className={styles.inputgroup}>
+              <input
+                type="text"
+                placeholder="LastName"
+                className={styles.inputfield}
+              />
+            </div>
+
+            <div className={styles.inputgroup}>
+              <img src="/models/images/user-solid.svg" alt="Username" className={styles.icon} />
+              <input
+                type="text"
+                placeholder="Username"
+                className={styles.inputfield}
+              />
+            </div>
+            <div className={styles.inputgroup}>
+              <img src="/models/images/email.jpeg" alt="Email" className={styles.icon} />
+              <input
+                type="email"
+                placeholder="Email"
+                className={styles.inputfield}
+              />
+            </div>
+            <div className={styles.inputgroup}>
+              <img src="/models/images/lock-solid.svg" alt="Password" className={styles.icon} />
+              <input
+                type="password"
+                placeholder="Password"
+                className={styles.inputfield}
+              />
+            </div>
+            <div className={styles.inputgroup}>
+              <img src="/models/images/lock-solid.svg" alt="Confirm Password" className={styles.icon} />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                className={styles.inputfield}
+              />
+            </div>
+
+            <button className={styles.signupbutton}>Sign Up</button>
+          </form>
+          <div className={styles.loginprompt}>
+            <p>
+              Already have an account?{' '}
+              <a href="/login" className={styles.loginlink}>
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
