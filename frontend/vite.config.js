06b2d059c8bd588b66ad96bac1e8ecb852351ced
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-02-22 17:01:34 +0545

    bild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,19 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // If you're using a specific library, such as react, separate it into its own chunk
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Create a separate vendor chunk for node_modules
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust this to change the warning threshold (in KB)
-  },
-};
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
