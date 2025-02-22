81525d6dfea00a06727a8b9f0f9f6965cb3e3c3a
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-02-22 18:33:21 +0545

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["dbms-genealogy.vercel.app"],  # Allow your frontend's origin
+    allow_origins=["*"],  # Allow your frontend's origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
