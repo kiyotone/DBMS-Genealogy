8baef9043c4af1b1ab71693422255dbcd8f6be25
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-02-25 15:03:11 +0545

    envs added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from .database import create_tables
 from .family.routes import router as family_router
 from .person.routes import router as person_router
@@ -7,19 +9,22 @@ from .events.routes import router as event_router
 from .relationships.routes import router as relationship_router
 from .users.routes import router as user_router
 
+# Load environment variables
+load_dotenv()
+
 app = FastAPI()
 
+# Get allowed origins from env variable and split them into a list
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
 
 # Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Add both local and deployed frontend
-    allow_credentials=True,  # This is key for cookies/auth headers
+    allow_origins=allowed_origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 
 @app.get("/")
 def read_root():
@@ -27,13 +32,9 @@ def read_root():
 
 create_tables()
 
-# Include family-related routes
+# Include routes
 app.include_router(family_router, prefix="/family", tags=["Family"])
-# Include person-related routes
 app.include_router(person_router, prefix="/person", tags=["Person"])
-# Include event-related routes
 app.include_router(event_router, prefix="/event", tags=["Event"])
-# Include relationship-related routes
 app.include_router(relationship_router, prefix="/relationship", tags=["Relationship"])
-# Include user-related routes
 app.include_router(user_router, prefix="/auth", tags=["User"])
